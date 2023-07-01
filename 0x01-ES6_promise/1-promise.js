48ee76d289bc92407b48b149ec2658eaca2e6b66
BaraqFi <mbarrack01@gmail.com>
2023-06-30 22:29:22 -0400

    progr

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(successs) {
   return new Promise((resolve, reject) => {
-    if (success) {
+   setTimeout(() => {
+    if (typeof success === 'boolean') {
 	resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
+   }, 2000);
   });
 }
-
-
